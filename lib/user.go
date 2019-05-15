ef97f36959808005cdb388fa62f75bd760c1dfda
hanxiao1 <hanxiao1@guazi.com>
2019-05-15 14:11:18 +0800

    add running

--- lib/user.go
+++ lib/user.go
@@ -11,7 +11,11 @@ func NewUser(name string) *User {
 }
 
 func (u *User) Read() {
-	fmt.Printf("User %s is reading....", u.name)
+	fmt.Printf("%s is reading...", u.name)
+}
+
+func (u *User) Run() {
+	fmt.Printf("%s is running...", u.name)
 }
 
 type Reader interface {
